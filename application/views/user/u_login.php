f3a37f649f6a12b0bd28c13f07bff6cc160b7e2a
Ricky <janorickyjohn@gmail.com>
2021-07-15 18:08:54 +0800

    upd 7/15

--- application/views/user/u_login.php
+++ application/views/user/u_login.php
@@ -3,81 +3,46 @@
 $template_header;
 ?>
 
-<body class="" style="background-color: rgba(241, 182, 171, 1);">
-	<?php $this->load->view("user/template/u_t_navbar"); ?>
-	<div class="container px-5 rounded pb-5" style="background-color: rgba(220, 138, 107, 0.40);">
-		<?php if ($this->session->flashdata("notice")): ?>
-			<?php $alert = $this->session->flashdata("notice"); ?>
-			<div class="alert alert-<?=$alert[0]?> alert-dismissible">
-				<?=$alert[1]?>
-				<button type="button" class="close" data-dismiss="alert">
-					&times;
-				</button>
-			</div>
-		<?php endif; ?>
-		<span>
-			<h1 class="m-0" style="padding-top: 60px;">Log-In</h1>
-		</span>
-		<div class="row mt-5">
-			<div class="col-4"><!-- this serves as margin --></div> 
-			<div class="col-4">
-				<div class="card text-center mt-5">
-					<div class="card-header">
-						<h3>LOG-IN</h3>
+<body>
+	<div class="wrapper bg">
+		<?php $this->load->view("user/template/u_t_navbar"); ?>
+		<div class="container-fluid">
+			<div class="row mb-4 mt-4">
+				<div class="col-0 col-lg-1"></div>
+				<div class="col-12 col-lg-10 content py-4">
+					<div class="row mt-4">
+						<div class="col-12 banner text-center">
+							<div class="banner_board">
+								<h5 class="font-weight-bold">Log-In</h5>
+							</div>
+						</div>
 					</div>
-					<div class="card-body">
-						<?php if ($this->session->flashdata("user_alert")): ?>
-							<?php $alert = $this->session->flashdata("user_alert"); ?>
-							<div class="alert alert-<?=$alert[0]?> alert-dismissible">
-								<?=$alert[1]?>
-								<button type="button" class="close" data-dismiss="alert">
-									&times;
-								</button>
-							</div>
-						<?php endif; ?>
-						<?=form_open(base_url() . "login_verify", "method='POST'")?>
-							<div class="form-group">
-								<label for="inp_email">Email:</label>
-								<input type="email" class="form-control" name="inp_email" placeholder="Email Address">
-							</div>
-							<div class="form-group">
-								<label for="inp_password">Password:</label>
-								<input type="password" class="form-control" name="inp_password" placeholder="Password">
-							</div>
-							<input type="submit" class="btn btn-primary" value="Log-In">
-						<?=form_close()?>
+					<div class="row mt-5">
+						<div class="col-1 col-md-3"></div>
+						<div class="col-10 col-md-6 text-center">
+							<?=form_open(base_url() . "login_verify", "method='POST'")?>
+								<div class="form-group">
+									<h5 class="font-weight-bold" for="inp_email">Email:</h5>
+									<input type="email" class="form-control" name="inp_email" placeholder="Email Address">
+								</div>
+								<div class="form-group">
+									<h5 class="font-weight-bold" for="inp_password">Password:</h5>
+									<input type="password" class="form-control" name="inp_password" placeholder="Password">
+								</div>
+								<input type="submit" class="btn btn-primary" value="Log-In">
+							<?=form_close()?>
+						</div>
+						<div class="col-1 col-md-3"></div>
+					</div>
+					<div class="row mt-5">
+						<div class="col-12 text-center">
+							Don't have an account? <a href="register">Register</a>
+						</div>
 					</div>
 				</div>
-				<a href="register">Register</a>
 			</div>
-			<div class="col-4"></div>
 		</div>
+		<?php $this->load->view("user/template/u_t_footer"); ?>
 	</div>
-	<footer style="background-color: white; height: auto;">
-		<div class="row mx-5 py-4">
-			<div class="col">
-				<h6 class="mb-2">Links</h6>
-				<ul class="nav flex-column">
-					<li><a class="text-dark" href="#">FAQs</a></li>
-					<li><a class="text-dark" href="#">About Us</a></li>
-					<li><a class="text-dark" href="#">Contact Us</a></li>
-					<li><a class="text-dark" href="#">Terms of Service</a></li>
-					<li><a class="text-dark" href="#">Privacy Policy</a></li>
-				</ul>
-			</div>
-			<div class="col">
-				<h6>Account</h6>
-			</div>
-			<div class="col">
-				<h6>Follow us on</h6>
-				<ul class="nav">
-					<li><a href="#">
-						<i class="fa fa-facebook-official bg dark" aria-hidden="true"></i></a></li>
-					<li><a href="#">
-						<i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
-				</ul>
-			</div>
-		</div>
-	</footer>
 </body>
 </html>
--- application/views/user/u_register.php
+++ application/views/user/u_register.php
@@ -3,168 +3,185 @@
 $template_header;
 ?>
 
-<body class="" style="background-color: rgba(241, 182, 171, 1);">
-	<?php $this->load->view("user/template/u_t_navbar"); ?>
-	<div class="container px-5 rounded pb-5" style="background-color: rgba(220, 138, 107, 0.40);">
-		<span>
-			<h1 class="m-0" style="padding-top: 60px;">Register</h1>
-		</span>
-		<div class="row mt-5">
-			<div class="col-3"></div> 
-			<div class="col-6">
-				<?=form_open(base_url() . "register_account", "method='POST'")?>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">Last Name: </h5>
-						</div>
-						<div class="col-9">
-							<input type="text" name="inp_name_last" placeholder="Last Name" autocomplete="off">
+<body>
+	<div class="wrapper bg">
+		<?php $this->load->view("user/template/u_t_navbar"); ?>
+		<div class="container-fluid">
+			<div class="row mb-4 mt-4">
+				<div class="col-0 col-lg-1"></div>
+				<div class="col-12 col-lg-10 content py-4">
+					<?=form_open(base_url() . "register_account", "method='POST'")?>
+					<div class="row mt-4">
+						<div class="col-12 banner text-center">
+							<div class="banner_board">
+								<h5 class="font-weight-bold">Register</h5>
+							</div>
 						</div>
 					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">First Name: </h5>
-						</div>
-						<div class="col-9">
-							<input type="text" name="inp_name_first" placeholder="First Name" autocomplete="off">
-						</div>
-					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">Middle Name: </h5>
-						</div>
-						<div class="col-9">
-							<input type="text" name="inp_name_middle" placeholder="Middle Name" autocomplete="off">
-						</div>
-					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">Name Extension: </h5>
-						</div>
-						<div class="col-9">
-							<input type="text" name="inp_name_extension" placeholder="e.g. Jr., Sr., etc." autocomplete="off">
-						</div>
-					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">Email: </h5>
-						</div>
-						<div class="col-9">
-							<input type="email" name="inp_email" placeholder="Email Address" autocomplete="off">
-						</div>
-					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">Password: </h5>
-						</div>
-						<div class="col-9">
-							<input type="password" name="inp_password" placeholder="Password" autocomplete="off">
+					<div class="row mt-5">
+						<div class="col-1 col-md-3"></div> 
+						<div class="col-10 col-md-6">
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">Last Name: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="text" name="inp_name_last" placeholder="Last Name" autocomplete="off">
+								</div>
+							</div>
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">First Name: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="text" name="inp_name_first" placeholder="First Name" autocomplete="off">
+								</div>
+							</div>
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">Middle Name: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="text" name="inp_name_middle" placeholder="Middle Name" autocomplete="off">
+								</div>
+							</div>
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">Name Extension: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="text" name="inp_name_extension" placeholder="e.g. Jr., Sr., etc." autocomplete="off">
+								</div>
+							</div>
 						</div>
 					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">Gender: </h5>
-						</div>
-						<div class="col-9">
-							<select name="inp_gender">
-								<option value="male">Male</option>
-								<option value="female">Female</option>
-								<option value="other">Other</option>
-							</select>
+					<div class="row mt-4">
+						<div class="col-12 banner text-center">
+							<div class="banner_board">
+								<h5 class="font-weight-bold">Update Account Info</h5>
+							</div>
 						</div>
 					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">Zip Code: </h5>
-						</div>
-						<div class="col-9">
-							<input type="text" name="inp_zip_code" placeholder="Zip Code" autocomplete="off">
-						</div>
-					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">Country: </h5>
-						</div>
-						<div class="col-9">
-							<input type="text" name="inp_country" placeholder="Country" autocomplete="off">
-						</div>
-					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">Province: </h5>
-						</div>
-						<div class="col-9">
-							<input type="text" name="inp_province" placeholder="Province" autocomplete="off">
-						</div>
-					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">City: </h5>
-						</div>
-						<div class="col-9">
-							<input type="text" name="inp_city" placeholder="City" autocomplete="off">
-						</div>
-					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">Street/Road: </h5>
-						</div>
-						<div class="col-9">
-							<input type="text" name="inp_street" placeholder="Street/Road" autocomplete="off">
+					<div class="row mt-5">
+						<div class="col-1 col-md-3"></div> 
+						<div class="col-10 col-md-6">
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">Email: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="email" name="inp_email" placeholder="Email Address" autocomplete="off">
+								</div>
+							</div>
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">Password: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="password" name="inp_password" placeholder="Password" autocomplete="off">
+								</div>
+							</div>
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">Gender: </h5>
+								</div>
+								<div class="col-9">
+									<select class="form-control" name="inp_gender">
+										<option value="male">Male</option>
+										<option value="female">Female</option>
+										<option value="other">Other</option>
+									</select>
+								</div>
+							</div>
 						</div>
 					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">House Number/Floor/Bldg./etc.: </h5>
-						</div>
-						<div class="col-9">
-							<input type="text" name="inp_address" placeholder="Address" autocomplete="off">
-						</div>
-					</div>
-					<div class="row mt-2">
-						<div class="col-3">
-							<h5 class="font-weight-normal m-0 p-0">Contact Num: </h5>
-						</div>
-						<div class="col-9">
-							<input type="text" name="inp_contact_num" placeholder="Contact #" autocomplete="off">
+					<div class="row mt-4">
+						<div class="col-12 banner text-center">
+							<div class="banner_board">
+								<h5 class="font-weight-bold">Update Address Info</h5>
+							</div>
 						</div>
 					</div>
-					<div class="row mt-2">
-						<div class="col-12">
-							<input type="submit" value="Register">
+					<div class="row mt-5">
+						<div class="col-1 col-md-3"></div> 
+						<div class="col-10 col-md-6">
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">Zip Code: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="text" name="inp_zip_code" placeholder="Zip Code" autocomplete="off">
+								</div>
+							</div>
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">Country: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="text" name="inp_country" placeholder="Country" autocomplete="off">
+								</div>
+							</div>
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">Province: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="text" name="inp_province" placeholder="Province" autocomplete="off">
+								</div>
+							</div>
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">City: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="text" name="inp_city" placeholder="City" autocomplete="off">
+								</div>
+							</div>
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">Street/Road: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="text" name="inp_street" placeholder="Street/Road" autocomplete="off">
+								</div>
+							</div>
+							<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">House Number/Floor/Bldg./etc.: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="text" name="inp_address" placeholder="Address" autocomplete="off">
+								</div>
+							</div>
 						</div>
 					</div>
-				<?=form_close()?>
-				<a href="register">Log-In</a>
+					<div class="row mt-4">
+						<div class="col-12 banner text-center">
+							<div class="banner_board">
+								<h5 class="font-weight-bold">Update Contact Info</h5>
+							</div>
+						</div>
+					</div>
+					<div class="row mt-5">
+						<div class="col-1 col-md-3"></div> 
+							<div class="col-10 col-md-6">
+								<div class="row mt-2">
+								<div class="col-3">
+									<h5 class="font-weight-bold">Contact Num: </h5>
+								</div>
+								<div class="col-9">
+									<input class="form-control" type="text" name="inp_contact_num" placeholder="Contact #" autocomplete="off">
+								</div>
+							</div>
+						</div>
+						<div class="col-1 col-md-3"></div> 
+						<div class="col-12 text-center"><input class="btn btn-primary" type="submit" value="Register"></div> 
+					</div>
+					<?=form_close()?>
+				</div>
 			</div>
-			<div class="col-3"></div>
 		</div>
+		<?php $this->load->view("user/template/u_t_footer"); ?>
 	</div>
-	<footer style="background-color: white; height: auto;">
-		<div class="row mx-5 py-4">
-			<div class="col">
-				<h6 class="mb-2">Links</h6>
-				<ul class="nav flex-column">
-					<li><a class="text-dark" href="#">FAQs</a></li>
-					<li><a class="text-dark" href="#">About Us</a></li>
-					<li><a class="text-dark" href="#">Contact Us</a></li>
-					<li><a class="text-dark" href="#">Terms of Service</a></li>
-					<li><a class="text-dark" href="#">Privacy Policy</a></li>
-				</ul>
-			</div>
-			<div class="col">
-				<h6>Account</h6>
-			</div>
-			<div class="col">
-				<h6>Follow us on</h6>
-				<ul class="nav">
-					<li><a href="#">
-						<i class="fa fa-facebook-official bg dark" aria-hidden="true"></i></a></li>
-					<li><a href="#">
-						<i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
-				</ul>
-			</div>
-		</div>
-	</footer>
 </body>
 </html>
